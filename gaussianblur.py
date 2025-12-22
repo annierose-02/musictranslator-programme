904e095bfa4279cc6859c9358c949922ca8ab61b
annierose-02 <anniethom0909@gmail.com>
2025-12-22 13:34:03 +0000

    Stave line detection using HoughLineP added

--- gaussianblur.py
+++ gaussianblur.py
@@ -1,4 +1,6 @@
 import cv2
+import numpy as np
+
 #Load image
 image = cv2.imread("images/test_image1.jpg")
 if image is None:
@@ -13,8 +15,18 @@ blur = cv2.GaussianBlur(gray_image,(5,5),0)
 #Add adaptive thresholding
 thresh_mean = cv2.adaptiveThreshold(blur, 255, cv2.ADAPTIVE_THRESH_GAUSSIAN_C,
                                     cv2.THRESH_BINARY_INV, 21 ,13)
-cv2.imshow("Threshold blur", thresh_mean)
+
+#Detecting stavelines
+edges = cv2.Canny(thresh_mean, 50, 150, apertureSize=3)
+lines = cv2.HoughLinesP(edges, rho=1 , theta=1*np.pi/180 , threshold =150, minLineLength=50, maxLineGap=35)
+
+for detectedline in lines:
+    x1,y1,x2,y2 = detectedline[0]
+    cv2.line(image,(x1,y1),(x2,y2),(0,255,0),)
+
+cv2.imshow("Line detected image",image)
 cv2.waitKey(0)
+
 
 
 
